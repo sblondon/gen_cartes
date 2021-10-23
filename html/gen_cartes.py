628bfccd0d6042bb420c92cf918c6fd29109a53c
Stéphane Blondon <stephane.blondon@gmail.com>
2021-10-23 23:27:32 +0200

    début génération automatique html

--- html/gen_cartes.py
+++ html/gen_cartes.py
@@ -3,11 +3,36 @@ from weasyprint import HTML, CSS
 from weasyprint.text.fonts import FontConfiguration
 
 
+CARTES = [
+    {
+        "gauche": "./icones/baseline_home_black_24dp.png",
+        "droite": "./icones/baseline_home_black_24dp.png",
+        "haut": "./icones/baseline_home_black_24dp.png",
+        "bas": "./icones/baseline_home_black_24dp.png",
+        "contenu": {"titre": "Admunsen", "image": "./icones/baseline_home_black_24dp.png"}
+    },
+    {
+        "gauche": "./icones/baseline_home_black_24dp.png",
+        "droite": "./icones/baseline_home_black_24dp.png",
+        "haut": "./icones/baseline_home_black_24dp.png",
+        "bas": "./icones/baseline_home_black_24dp.png",
+        "contenu": {"titre": "Admunsen", "image": "./icones/baseline_home_black_24dp.png"}
+    },
+    {
+        "gauche": "./icones/baseline_home_black_24dp.png",
+        "droite": "./icones/baseline_home_black_24dp.png",
+        "haut": "./icones/baseline_home_black_24dp.png",
+        "bas": "./icones/baseline_home_black_24dp.png",
+        "contenu": {"titre": "Admunsen", "image": "./icones/baseline_home_black_24dp.png"}
+    },
+]
+
+
 
 def create_html():
     jinja_content = open("html/index.jinja.html").read()
     template = Template(jinja_content)
-    html_content = template.render()
+    html_content = template.render(cartes=CARTES)
     open("html/index.html", "w").write(html_content)
 
 
