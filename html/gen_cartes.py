9f7152454b8709fc8da231f110e7f2caae3030fe
Stéphane Blondon <stephane.blondon@gmail.com>
2021-10-25 14:36:28 +0200

    découpage des cartes par ligne de 3

--- html/gen_cartes.py
+++ html/gen_cartes.py
@@ -16,23 +16,32 @@ CARTES = [
         "droite": "./icones/baseline_home_black_24dp.png",
         "haut": "./icones/baseline_home_black_24dp.png",
         "bas": "./icones/baseline_home_black_24dp.png",
-        "contenu": {"titre": "Admunsen", "image": "./icones/baseline_home_black_24dp.png"}
+        "contenu": {"titre": "AA", "image": "./icones/baseline_home_black_24dp.png"}
     },
     {
         "gauche": "./icones/baseline_home_black_24dp.png",
         "droite": "./icones/baseline_home_black_24dp.png",
         "haut": "./icones/baseline_home_black_24dp.png",
         "bas": "./icones/baseline_home_black_24dp.png",
-        "contenu": {"titre": "Admunsen", "image": "./icones/baseline_home_black_24dp.png"}
+        "contenu": {"titre": "BB", "image": "./icones/baseline_home_black_24dp.png"}
     },
 ]
 
 
+def _cartes_par_lignes(cartes):
+    CARTES_PAR_LIGNE = 3
+    return [
+        cartes[i:i+CARTES_PAR_LIGNE]
+        for i
+        in range(0, len(cartes), CARTES_PAR_LIGNE)
+    ]
+
+    return cartes
 
 def create_html():
     jinja_content = open("html/index.jinja.html").read()
     template = Template(jinja_content)
-    html_content = template.render(cartes=CARTES)
+    html_content = template.render(lignes_cartes=_cartes_par_lignes(CARTES))
     open("html/index.html", "w").write(html_content)
 
 
