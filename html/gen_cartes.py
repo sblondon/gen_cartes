f54c439caf5f51ffb7a76ef19a8a9eb4f156a725
Stéphane Blondon <stephane.blondon@gmail.com>
2021-11-20 18:08:08 +0100

    refacto: icones dans un dictionnaire

--- html/gen_cartes.py
+++ html/gen_cartes.py
@@ -9,50 +9,53 @@ ILLUSTRATIONS = {
     "exploration": "../images/illustration_exploration.png",
     "preparation": "../images/illustration_preparation.png",
 }
-iconeVide = "../images/icones/vide_black_24dp.png"
-iconeEffet = "../material-design-icons/png/navigation/east/materialicons/24dp/2x/baseline_east_black_24dp.png"
-iconeOu = "../images/icones/ou_black_24dp.png"
-iconeAutresJoueurs = "../material-design-icons/png/social/groups/materialicons/24dp/2x/baseline_groups_black_24dp.png"
-iconeSacADos = "../material-design-icons/png/places/backpack/materialicons/24dp/2x/baseline_backpack_black_24dp.png"
-iconeCapitaine = "../images/icones/capitaine.png"
-iconeBateau = "../material-design-icons//png/maps/directions_boat/materialiconsround/24dp/2x/round_directions_boat_black_24dp.png"
-iconeChien = "../material-design-icons/png/action/pets/materialicons/24dp/2x/baseline_pets_black_24dp.png"
-iconeFinChien = "../images/icones/baseline_pets_off_black_24dp.png"
-iconeNourriture = "../material-design-icons/png/maps/restaurant/materialicons/24dp/2x/baseline_restaurant_black_24dp.png"
-iconeFinNourriture = "../material-design-icons/png/maps/no_meals/materialicons/24dp/2x/baseline_no_meals_black_24dp.png"
-iconeTraineau = "../material-design-icons/png/action/shopping_cart/materialicons/24dp/2x/baseline_shopping_cart_black_24dp.png"
-iconeFinTraineau = "../material-design-icons/png/action/remove_shopping_cart/materialicons/24dp/2x/baseline_remove_shopping_cart_black_24dp.png"
-iconeHomme = "../material-design-icons/png/social/person/materialicons/24dp/2x/baseline_person_black_24dp.png"
-iconeFinHomme = "../images/icones/baseline_person_off_black_24dp.png"
-iconeArme = "../material-design-icons/png/hardware/security/materialicons/24dp/2x/baseline_security_black_24dp.png"
-iconeVetement = "../images/icones/beanie.png"
-iconeExplore = "../material-design-icons/png/action/explore/materialicons/24dp/2x/baseline_explore_black_24dp.png"
-iconeDeplacement = "../material-design-icons/png/maps/directions_walk/materialicons/24dp/2x/baseline_directions_walk_black_24dp.png"
-iconeFinDeplacement = "../images/icones/walk_off.png"
-iconeTempete = "../material-design-icons/png/places/ac_unit/materialicons/24dp/2x/baseline_ac_unit_black_24dp.png"
+
+ICONES = {
+    "vide": "../images/icones/vide_black_24dp.png",
+    "effet": "../material-design-icons/png/navigation/east/materialicons/24dp/2x/baseline_east_black_24dp.png",
+    "ou": "../images/icones/ou_black_24dp.png",
+    "autresJoueurs": "../material-design-icons/png/social/groups/materialicons/24dp/2x/baseline_groups_black_24dp.png",
+    "sacADos": "../material-design-icons/png/places/backpack/materialicons/24dp/2x/baseline_backpack_black_24dp.png",
+    "capitaine": "../images/icones/capitaine.png",
+    "bateau": "../material-design-icons//png/maps/directions_boat/materialiconsround/24dp/2x/round_directions_boat_black_24dp.png",
+    "chien": "../material-design-icons/png/action/pets/materialicons/24dp/2x/baseline_pets_black_24dp.png",
+    "finChien": "../images/icones/baseline_pets_off_black_24dp.png",
+    "nourriture": "../material-design-icons/png/maps/restaurant/materialicons/24dp/2x/baseline_restaurant_black_24dp.png",
+    "finNourriture": "../material-design-icons/png/maps/no_meals/materialicons/24dp/2x/baseline_no_meals_black_24dp.png",
+    "traineau": "../material-design-icons/png/action/shopping_cart/materialicons/24dp/2x/baseline_shopping_cart_black_24dp.png",
+    "finTraineau": "../material-design-icons/png/action/remove_shopping_cart/materialicons/24dp/2x/baseline_remove_shopping_cart_black_24dp.png",
+    "homme": "../material-design-icons/png/social/person/materialicons/24dp/2x/baseline_person_black_24dp.png",
+    "finHomme": "../images/icones/baseline_person_off_black_24dp.png",
+    "arme": "../material-design-icons/png/hardware/security/materialicons/24dp/2x/baseline_security_black_24dp.png",
+    "vetement": "../images/icones/beanie.png",
+    "explore": "../material-design-icons/png/action/explore/materialicons/24dp/2x/baseline_explore_black_24dp.png",
+    "deplacement": "../material-design-icons/png/maps/directions_walk/materialicons/24dp/2x/baseline_directions_walk_black_24dp.png",
+    "finDeplacement": "../images/icones/walk_off.png",
+    "tempete": "../material-design-icons/png/places/ac_unit/materialicons/24dp/2x/baseline_ac_unit_black_24dp.png",
+}
 
 def _cartes():
     return [
     {
-        "gauche": iconeVetement,
-        "droite": iconeVetement,
-        "haut": iconeFinHomme,
-        "bas": iconeFinChien,
-        "contenu": {"titre": "Admunsen", "image": iconeCapitaine, "fond": ILLUSTRATIONS["preparation"]}
+        "gauche": ICONES["vetement"],
+        "droite": ICONES["vetement"],
+        "haut": ICONES["finHomme"],
+        "bas": ICONES["finChien"],
+        "contenu": {"titre": "Admunsen", "image": ICONES["capitaine"], "fond": ILLUSTRATIONS["preparation"]}
     },
     {
-        "gauche": iconeFinDeplacement,
-        "droite": iconeArme,
-        "haut": iconeSacADos,
-        "bas": iconeDeplacement,
-        "contenu": {"titre": "AA", "image": iconeArme, "fond": ILLUSTRATIONS["exploration"]}
+        "gauche": ICONES["finDeplacement"],
+        "droite": ICONES["arme"],
+        "haut": ICONES["sacADos"],
+        "bas": ICONES["deplacement"],
+        "contenu": {"titre": "AA", "image": ICONES["arme"], "fond": ILLUSTRATIONS["exploration"]}
     },
     {
-        "gauche": iconeDeplacement,
-        "droite": iconeDeplacement,
-        "haut": iconeDeplacement,
-        "bas": iconeDeplacement,
-        "contenu": {"titre": "BB", "image": iconeArme, "fond": ILLUSTRATIONS["preparation"]}
+        "gauche": ICONES["deplacement"],
+        "droite": ICONES["deplacement"],
+        "haut": ICONES["deplacement"],
+        "bas": ICONES["deplacement"],
+        "contenu": {"titre": "BB", "image": ICONES["arme"], "fond": ILLUSTRATIONS["preparation"]}
     },
 ]
 
